53cce3368242473f4c4eeab0300f4a314f440e03
aneel-56 <90965632+aneel-56@users.noreply.github.com>
2023-05-30 07:46:40 +0545

    Add files via upload

---
+++ src/mongodb.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb+srv://projectvi:projectvi@cluster0.hjkoc6m.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+}) 
+
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection;
